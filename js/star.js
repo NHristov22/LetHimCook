2a268d846f8b0ec3a41e7b226f55c67505298193
NHristov22 <167108571+NHristov22@users.noreply.github.com>
2024-05-19 23:13:17 +0300

    Update star.js

--- js/star.js
+++ js/star.js
@@ -1,1 +1,45 @@
+let stars = document.getElementsByClassName("star");
+let outputRaiting = document.getElementById("outputRating");
 
+function setRating(n)
+{
+    rem();
+
+    for (let i = 0; i < n; i++)
+    {
+        if (n == 1)
+            clas = "one";
+        else
+        {
+            if (n == 2)
+                clas = "two";
+            else
+            {
+                if (n == 3)
+                    clas = "three";
+                else
+                {
+                    if (n == 4)
+                        clas = "four";
+                    else
+                    {
+                        if (n == 5)
+                            clas = "five";
+                    }
+                }
+            }
+        }
+
+        stars[i].className = "star " + clas;
+    }
+
+    outputRaiting.innerText = n + " / 5";
+}
+
+function rem()
+{
+    for (let i = 0; i < 5; i++)
+    {
+        stars[i].className = "star";
+    }
+}
